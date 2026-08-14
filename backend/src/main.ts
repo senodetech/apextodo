78f15fa333156acbe346fdafed9de0bbdfd17d72
senodetech <senodetech@gmail.com>
2026-08-14 13:28:29 +0530

    fix: resolve CORS policy error and connect backend to PostgreSQL database

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
